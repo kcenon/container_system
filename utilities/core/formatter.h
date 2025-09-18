0c96678195410302ebb411e46d4ed0ebb7f77360
kcenon <kcenon@naver.com>
2025-09-19 00:47:20 +0900

    fix(utilities): add conditional compilation for std::format and fmt::format in formatter.h

--- utilities/core/formatter.h
+++ utilities/core/formatter.h
@@ -2,19 +2,28 @@
 
 #include <string>
 #include <sstream>
-#include <fmt/format.h>
+
+#ifdef USE_STD_FORMAT
+    #include <format>
+#else
+    #include <fmt/format.h>
+#endif
 
 namespace utility_module {
 
 /**
- * @brief Simple formatter wrapper around fmt library
+ * @brief Simple formatter wrapper around fmt library or std::format
  */
 class formatter {
 public:
     template<typename... Args>
     static std::string format(const std::string& format_str, Args&&... args) {
         try {
+#ifdef USE_STD_FORMAT
+            return std::vformat(format_str, std::make_format_args(args...));
+#else
             return fmt::vformat(format_str, fmt::make_format_args(args...));
+#endif
         } catch (const std::exception&) {
             return format_str; // Return original string if formatting fails
         }
@@ -23,7 +32,11 @@ public:
     template<typename OutputIt, typename... Args>
     static void format_to(OutputIt out, const std::string& format_str, Args&&... args) {
         try {
+#ifdef USE_STD_FORMAT
+            std::vformat_to(out, format_str, std::make_format_args(args...));
+#else
             fmt::vformat_to(out, format_str, fmt::make_format_args(args...));
+#endif
         } catch (const std::exception&) {
             // Fallback: just copy the format string
             std::copy(format_str.begin(), format_str.end(), out);
