9eb87434509d8aaf21d13164c9039d9eb4787a0f
kcenon <kcenon@naver.com>
2026-01-21 22:18:58 +0900

    fix(core): add missing <mutex> header for std::scoped_lock and std::unique_lock
    Fixes CI failure in Code Coverage workflow caused by missing header.

--- core/policy_container.h
+++ core/policy_container.h
@@ -97,6 +97,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endif
 
 #include <memory>
+#include <mutex>
 #include <string_view>
 #include <shared_mutex>
 #include <optional>
